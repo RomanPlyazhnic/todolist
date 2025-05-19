495f4d93d0a169e3992ae71a929ae7d1421a504d
RomanPlyazhnic <romanplyazhnic@gmail.com>
2025-05-19 16:05:25 +0300

    setup config

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/RomanPlyazhnic/todolist/internal/config"
 	"github.com/RomanPlyazhnic/todolist/internal/server"
 )
 
 func main() {
-	cfg := config.New()
+	cfg := setupConfig()
+
 	server.Run(cfg)
+}
+
+func setupConfig() *config.Data {
+	var cfgPath string
+	flag.StringVar(&cfgPath, "config", "", "config path")
+	flag.Parse()
+
+	var cfg *config.Data
+	if cfgPath != "" {
+		cfg = config.FromPath(cfgPath)
+	} else {
+		cfg = config.FromEnv()
+	}
+
+	return cfg
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,23 +1,43 @@
 package config
 
-import "time"
+import (
+	"fmt"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
 
 type Data struct {
-	Name     string
-	Protocol string // "rest" or "grpc"
-	Port     int
-	Timeout  time.Duration
-	Env      string
-	Version  string
+	Name     string        `yaml:"name" env:"NAME" env-default:"todolist"`
+	Protocol string        `yaml:"protocol" env:"PROTOCOL" env-default:"rest"` // "rest" or "grpc"
+	Port     int           `yaml:"port" env:"PORT" env-default:"8080"`
+	Timeout  time.Duration `yaml:"timeout" env:"TIMEOUT" env-default:"10s"`
+	Env      string        `yaml:"env" env:"ENV" env-default:"todolist"`
+	Version  string        `yaml:"version" env:"VERSION" env-default:"0.0.1"`
 }
 
-func New() *Data {
-	return &Data{
-		Name:     "TODOList",
-		Protocol: "rest",
-		Port:     8080,
-		Timeout:  10 * time.Second,
-		Env:      "dev",
-		Version:  "v0.0.1",
+func FromPath(path string) *Data {
+	const op = "config.New"
+
+	var cfg Data
+
+	err := cleanenv.ReadConfig(path, &cfg)
+	if err != nil {
+		panic(fmt.Errorf("%s: %w", op, err))
 	}
+
+	return &cfg
+}
+
+func FromEnv() *Data {
+	const op = "config.New"
+
+	var cfg Data
+
+	err := cleanenv.ReadEnv(&cfg)
+	if err != nil {
+		panic(fmt.Errorf("%s: %w", op, err))
+	}
+
+	return &cfg
 }
