1100521cb3f06935a5218e6df269c07b055ba5b0
RomanPlyazhnic <romanplyazhnic@gmail.com>
2025-06-05 15:12:58 +0300

    test login/register happy path

--- tests/functional/login_test.go
+++ tests/functional/login_test.go
@@ -3,7 +3,7 @@ package functional
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/RomanPlyazhnic/todolist/internal/app/contracts"
+	"github.com/go-faker/faker/v4"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"strings"
@@ -12,23 +12,80 @@ import (
 	"github.com/RomanPlyazhnic/todolist/tests/suite"
 )
 
-func TestLogin_LoginHappyPath(t *testing.T) {
+type RegisterRequest struct {
+	Username string `faker:"username"`
+	Password string `faker:"password"`
+}
+
+type LoginRequest struct {
+	Username string `faker:"username"`
+	Password string `faker:"password"`
+}
+
+func TestLogin_RegisterLoginHappyPath(t *testing.T) {
 	s := suite.New(t)
 
-	url := fmt.Sprintf("http://%s:%d/Register", s.App.Config.Domain, s.App.Config.Port)
-	s.App.Logger.Info(url)
-
-	r := contracts.RegisterRequest{
-		Username: "testuser",
-		Password: "testpassword",
+	// Register user
+	r := RegisterRequest{}
+	err := faker.FakeData(&r)
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
 	}
 	jsonStr, err := json.Marshal(r)
-	body := strings.NewReader(string(jsonStr))
-	resp, err := http.Post(url, "application/json", body)
+	reqBody := strings.NewReader(string(jsonStr))
 
+	url := fmt.Sprintf("http://%s:%d/Register", s.App.Config.Domain, s.App.Config.Port)
+	resp, err := http.Post(url, "application/json", reqBody)
+	closeResp(t, resp)
 	if err != nil {
 		t.Fatalf("Expected no error, got %v", err)
 	}
 
 	assert.Equal(t, 200, resp.StatusCode)
+
+	// Login registered user
+	l := LoginRequest{
+		Username: r.Username,
+		Password: r.Password,
+	}
+	jsonStr, err = json.Marshal(l)
+	reqBody = strings.NewReader(string(jsonStr))
+
+	url = fmt.Sprintf("http://%s:%d/Login", s.App.Config.Domain, s.App.Config.Port)
+	resp, err = http.Post(url, "application/json", reqBody)
+	closeResp(t, resp)
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+
+	assert.Equal(t, 200, resp.StatusCode)
+
+	// Access service by logined user
+	client := &http.Client{}
+	url = fmt.Sprintf("http://%s:%d", s.App.Config.Domain, s.App.Config.Port)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+	cookie := resp.Cookies()[0]
+	req.AddCookie(cookie)
+
+	resp, err = client.Do(req)
+	defer closeResp(t, resp)
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+
+	assert.Equal(t, 200, resp.StatusCode)
+}
+
+func closeResp(t *testing.T, resp *http.Response) {
+	if resp == nil {
+		return
+	}
+
+	err := resp.Body.Close()
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
 }
